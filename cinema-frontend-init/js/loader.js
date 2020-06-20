05c410c12b7bee318e07dcf8066f2ea93c0f3614
melihcanclk <melihcanclk@hotmail.com>
2020-06-20 19:41:10 +0300

    feat(cinema-frontend-init) : Loading animation -Loading animation at js file and it can be used by using at one html id -After loading animation finished, Cinema Saloon choosing page will be seen

---
+++ cinema-frontend-init/js/loader.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    let myVar;
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
